580597653038bf59180525919d3c3ab19785ed0d
Hikvisionsale <hikvisionmarket@163.com>
2026-06-05 17:29:03 +0800

    Add registration and login views with corresponding URL paths

--- hello/views.py
+++ hello/views.py
@@ -5,4 +5,10 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django World!")
+    return HttpResponse("Hello, Django World! Vincent")
+
+def register(request):
+    return HttpResponse("This is the <bold>Registration</bold> page.")
+
+def login(request):
+    return HttpResponse("This is the <bold>Login</bold> page.")
